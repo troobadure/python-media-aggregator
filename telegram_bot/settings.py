dd502a5e387a5251e5ee78a030acad01aee13b21
vladShadow <vladivashchuk@gmail.com>
2021-05-23 16:35:18 +0300

    remove db

--- telegram_bot/settings.py
+++ telegram_bot/settings.py
@@ -2,7 +2,7 @@ import os
 
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-DATABASE_URL = os.getenv('DATABASE_URL')
+# DATABASE_URL = os.getenv('DATABASE_URL')
 if not BOT_TOKEN:
     print('You have not set BOT_TOKEN')
     quit()
