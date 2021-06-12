3a8ad85171e35a6c8cb4cb2d6d04b2f0f9d044dd
vladShadow <vladivashchuk@gmail.com>
2021-06-12 17:18:13 +0300

    fix registration

--- telegram_bot/handlers/registration.py
+++ telegram_bot/handlers/registration.py
@@ -7,12 +7,12 @@ def attach(bot, db_manager):
     def start_message(message):
         user_name = message.from_user.full_name
 
-        bot.send_message(message.chat.id,
-                        f"Hellol, {user_name}",
-                        reply_markup=keyboards.main_menu_keyboard)
-
         db_manager.register_user(message.chat.id,
                                 user_name,
                                 'main_menu',
                                 time.strftime('%d/%m/%y, %X'),
                                 time.strftime('%d/%m/%y, %X'))
+
+        bot.send_message(message.chat.id,
+                        f"Hellol, {user_name}",
+                        reply_markup=keyboards.main_menu_keyboard)
